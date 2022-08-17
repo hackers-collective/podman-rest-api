108dbf10a1a275e4186cce3378ee174738edc4ea
Shobhit Sharma <shobhit0812@gmail.com>
2022-08-17 08:45:18 +0530

    Update podman-rest.sh
    Auto kill existing process & generate new PID

--- podman-rest.sh
+++ podman-rest.sh
@@ -2,4 +2,6 @@
 
 # Podman REST API Service'
 
-sudo podman system service --time=0 tcp::7181 & # Change the port number accordingly
+kill $(pidof "podman")
+
+podman system service --time=0 tcp::7181 & # Change the port number accordingly
