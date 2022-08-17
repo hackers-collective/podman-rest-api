bed05548a71fee650676470e824c49fa1f5dbe81
Shobhit Sharma <shobhit0812@gmail.com>
2022-08-17 07:25:46 +0530

    Update podman-rest.sh

--- podman-rest.sh
+++ podman-rest.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo 'Starting Podman REST API Service'
+# Podman REST API Service'
 
-sudo podman system service --time=0 tcp::7181 & // Change the port number accordingly
+sudo podman system service --time=0 tcp::7181 & # Change the port number accordingly
